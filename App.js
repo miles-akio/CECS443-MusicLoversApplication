45442fc237d44bca022770807eaed802f38ff08f
ChrisPCodes <61615462+ChrisPCodes@users.noreply.github.com>
2023-09-04 17:58:45 -0700

    Navigation set up this will overide the current UI that we have (Note Firebase not currently linked with this update)

--- App.js
+++ App.js
@@ -1,115 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, button, Pressable, TextInput } from 'react-native';
+// In App.js in a new project
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import * as React from 'react';
+import { View, Text } from 'react-native';
 
-import { getDatabase, ref, set, update, onValue, remove, get, child } from "firebase/database";
-import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAq3F_9cn_1tJnNP1S3Soao4TCc79dOwaE",
-  authDomain: "mind-bridge-a93eb.firebaseapp.com",
-  projectId: "mind-bridge-a93eb",
-  storageBucket: "mind-bridge-a93eb.appspot.com",
-  messagingSenderId: "1052298113718",
-  appId: "1:1052298113718:web:2f3f42634a541b55f7ac17"
-};
+import LandingScreen from './components/auth/Landing'
+import { createStackNavigator } from '@react-navigation/stack';
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getDatabase();
-
-export default function App() {
-
-
-  const [username, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [lastUserName, setLastUserName] = useState('');
-
-  function createData() {
-
-    set(ref(db, 'users/' + username), {          
-      username: username,
-      email: email  
-    }).then(() => {
-      // Data saved successfully!
-      alert('data created!');    
-  })  
-      .catch((error) => {
-          // The write failed...
-          alert(error);
-      });
-}
-
-  function getData() {
-    const dbRef = ref(getDatabase());
-    get(child(dbRef, `users/${username}`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      var data = snapshot.val()
-      console.log(data);
-      alert('data: ' + JSON.stringify(data))
-    } else {
-      console.log("No data available");
-    }
-    }).catch((error) => {
-      console.error(error);   
-    });  
-  }
-
-
+const Stack = createStackNavigator();
+function App() {
   return (
-
-  <View style={styles.container}>
-      <Text>firebase</Text>
-      <TextInput value={username} 
-      onChangeText={(username) => {
-          setName(username)
-          setLastUserName(username) // add this line
-        }
-      } 
-      placeholder='Username' style={styles.TextBoxes}></TextInput>
-      <TextInput value={email} onChangeText={(email) => {setEmail(email)}} placeholder='Email' style={styles.TextBoxes}></TextInput>
-      <Button title='create data' onPress={createData}></Button>
-      <Button title='get data' onPress={getData}></Button>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+      <Stack.Screen name ="Landing" component = {LandingScreen} options={{headerShown:true}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button:{
-    marginTop: 10,
-    marginBottom: 10,
-    height: 47,
-    borderRadius:5,
-    backgroundColor: '#788ecc',
-    width:200,
-    alignItems:'center',
-    justifyContent:'center',
- },
- buttonText:{
-    color:'white',
-    fontSize:15,
- },
- TextBoxes: {
-  width:'90%',
-  fontSize:18,
-  padding:12,
-  backgroundColor:'grey',
-  marginVertical:10,
-}
-
-});
+export default App;
---
+++ components/auth/Register.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import { View,Button,TextInput } from 'react-native'
+
+export class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Register
