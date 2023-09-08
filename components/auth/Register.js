b554c29ad4e72020a8e2b3a482c1ae37961ce8e5
jmessina <j.w.messina24@gmail.com>
2023-09-08 11:33:54 -0700

    Added TextInput views with styling and padding to Regidster.js

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { View, Button, TextInput } from 'react-native' 
+import { View, Button, TextInput, StyleSheet } from 'react-native' 
 // TODO: Resolve Firebase imports 
 import firebase from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
@@ -33,35 +33,48 @@ export class Register extends Component {
     }
 
 
-  render() {
-    return (
-      <View>
-        <TextInput 
-            // When empty this is the value thats filled
-            placeholder = " name"
-            // JS assumes that the argugment we pass is same as the variable we created 
-            onChangeText={(name)=> this.setState({name})}
-        />
-
-        <TextInput 
-            placeholder = " email" 
-            onChangeText={(name)=> this.setState({email})}
-        />
-
-        <TextInput 
-            placeholder = " password"
-            secureEntry = {true} // secures passowrd text 
-            onChangeText={(name)=> this.setState({password})}
-        />
-
-        <Button
-            onPress ={()=> this.onSignUp()}
-            title = "Sign Up"
-         />
-        
-      </View>
-    )
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <TextInput
+              // Formatting the appearance
+              style={styles.input} 
+              // When empty this is the value thats filled
+              placeholder = " Name"
+              // JS assumes that the argugment we pass is same as the variable we created 
+              onChangeText={(name)=> this.setState({name})}
+          />
+  
+          <TextInput
+              style={styles.input} 
+              placeholder = " Email" 
+              onChangeText={(name)=> this.setState({email})}
+          />
+  
+          <TextInput 
+              style={styles.input}
+              placeholder = " password"
+              secureEntry = {true} // secures passowrd text 
+              onChangeText={(name)=> this.setState({password})}
+          />
+  
+          <Button
+              onPress ={()=> this.onSignUp()}
+              title = "Sign Up"
+           />
+          
+        </View>
+      )
+    }
   }
-}
+  
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
 
 export default Register;
