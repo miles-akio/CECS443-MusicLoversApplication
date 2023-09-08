b5ae76f198a52f740c04dce6554c8bcf3b8419ce
ChrisPCodes <61615462+ChrisPCodes@users.noreply.github.com>
2023-09-08 14:01:14 -0700

    FireBase set up with authenitcation

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
 import { View, Button, TextInput } from 'react-native' 
 // TODO: Resolve Firebase imports 
-import firebase from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import { createUserWithEmailAndPassword, getAuth } from '@firebase/auth'; 
 
 //With the necessary imports to create Register page 
 export class Register extends Component {
@@ -15,43 +15,42 @@ export class Register extends Component {
             name: ''
         }
 
-        this.onSingUp = this.onSingUp.bind(this) // Need to bind to give access to components state variables 
+        this.onSignUp = this.onSignUp.bind(this) // Need to bind to give access to components state variables 
     }
 
     // TODO: Resolve Firebase function calls and imports 
-    onSingUp(){
-        /* 
+    onSignUp(){
+        const auth = FIREBASE_AUTH;
         const { email, password, name } = this.state;
-        firebase.auth().createUserWithEmailAndPassword(email,password)
+        createUserWithEmailAndPassword(auth,email,password)
         .then((result) => {
             console.log(result)
         })
         .catch((error) => {
             console.log(error)
-        })   
-        */
+        })    
+        
     }
-
 
   render() {
     return (
       <View>
         <TextInput 
             // When empty this is the value thats filled
-            placeholder = " name"
+            placeholder = "name"
             // JS assumes that the argugment we pass is same as the variable we created 
             onChangeText={(name)=> this.setState({name})}
         />
 
         <TextInput 
-            placeholder = " email" 
-            onChangeText={(name)=> this.setState({email})}
+            placeholder = "email" 
+            onChangeText={(email)=> this.setState({email})}
         />
 
         <TextInput 
-            placeholder = " password"
+            placeholder = "password"
             secureEntry = {true} // secures passowrd text 
-            onChangeText={(name)=> this.setState({password})}
+            onChangeText={(password)=> this.setState({password})}
         />
 
         <Button
